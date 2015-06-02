b34c272816f1aa38758de4313676a335ed646ad6
nitesis <viviane.bendjus@students.fhnw.ch>
2015-06-02 13:53:26 +0200

    finished servlet agap

--- ChatRoomPrj/src/ch/fhnw/kvan/chat/servlet/client/Client.java
+++ ChatRoomPrj/src/ch/fhnw/kvan/chat/servlet/client/Client.java
@@ -1,5 +1,191 @@
 package ch.fhnw.kvan.chat.servlet.client;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import ch.fhnw.kvan.chat.interfaces.IChatRoom;
+
+public class Client implements ch.fhnw.kvan.chat.interfaces.IChatRoom, ch.fhnw.kvan.chat.interfaces.IChatDriver{
+
+	
+	private String screenName;
+
+	public Client(String screenName) {
+		this.screenName = screenName;
+	}
+	
+
+	@Override
+	public boolean addParticipant(String name) throws IOException {
+		final String USER_AGENT = "Mozilla/5.0";
+		String url = "http://localhost:8080/chat/?action=addParticipant&name=" + name;
+		 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		return true;
+	}
+
+	@Override
+	public boolean removeParticipant(String name) throws IOException {
+		final String USER_AGENT = "Mozilla/5.0";
+		String url = "http://localhost:8080/chat/?action=removeParticipant&name=" + name;
+		 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+		disconnect();
+		return true;
+	}
+
+	@Override
+	public boolean addTopic(String topic) throws IOException {
+		final String USER_AGENT = "Mozilla/5.0";
+		String url = "http://localhost:8080/chat/?action=addTopic&topic=" + topic;
+		 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		return true;
+	}
+
+	@Override
+	public boolean removeTopic(String topic) throws IOException {
+		final String USER_AGENT = "Mozilla/5.0";
+		String url = "http://localhost:8080/chat/?action=removeTopic&topic=" + topic;
+		 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		return true;
+	}
+
+	@Override
+	public boolean addMessage(String topic, String message) throws IOException {
+		String currentMessage = ("[" + screenName + "]: " + message);
+		//currentMessage = "message=" + currentMessage + ";topic=" + topic;
+		
+		final String USER_AGENT = "Mozilla/5.0";
+		String url = "http://localhost:8080/chat/?action=postMessage&message=" + currentMessage + "&topic=" + topic;
+		 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+		return true;
+	}
+
+	@Override
+	public String getMessages(String topic) throws IOException {
+		final String USER_AGENT = "Mozilla/5.0";
+		String url = "http://localhost:8080/chat/?action=getMessage&topic=" + topic;
+		 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		return response.toString();
+	}
+
+	@Override
+	public String refresh(String topic) throws IOException {
+		final String USER_AGENT = "Mozilla/5.0";
+		String url = "http://localhost:8080/chat/?action=refresh&topic=" + topic;
+		 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		return response.toString();
+	}
+
+	@Override
+	public void connect(String host, int port) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnect() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IChatRoom getChatRoom() {
+		return this;
+	}
 }
--- ChatRoomPrj/src/ch/fhnw/kvan/chat/servlet/server/ChatServer.java
+++ ChatRoomPrj/src/ch/fhnw/kvan/chat/servlet/server/ChatServer.java
@@ -9,13 +9,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.*;
 
+import ch.fhnw.kvan.chat.general.ChatRoom;
 import ch.fhnw.kvan.chat.general.ChatRoomDriver;
 
-//@WebServlet("/chat")
+@WebServlet("/chat")
 public class ChatServer extends HttpServlet{
 
-	ChatRoomDriver crd = new ChatRoomDriver();
+	private ChatRoomDriver crd = new ChatRoomDriver();
+	private ChatRoom chatRoom;
 	
+	private PrintWriter out;
+	private String message; 
 	public void init() throws ServletException {
 		
 		crd.connect("localhost", 8080);
@@ -24,16 +28,68 @@ public class ChatServer extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
 	{
+		String req = request.getParameter("action");
 		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title> Hallo Welt</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<h1> Hallo Welt</h1>");
-		out.println("Herzlich willkommen!");
-		out.println("</body></html>");
+		out = response.getWriter();
+		if ("addParticipant".equals(req)) {
+			// Add user to ChatRoom
+			boolean success = false;
+			success = chatRoom.addParticipant(request.getParameter("name"));
+			
+			if (success) {
+				out.write(chatRoom.getTopics());
+				out.write(chatRoom.getParticipants());
+				setMessage("addParticipant" + request.getParameter("name"));
+			}
+			
+		} else if ("removeParticipant".equals(req)) {
+			// Remove user from ChatRoom
+			boolean success = chatRoom.removeParticipant(request.getParameter("name"));
+			
+			if (success) {
+				setMessage("removeParticipant" + request.getParameter("name"));
+			}
+				
+		} else if ("addTopic".equals(req)) {
+			boolean success = chatRoom.addTopic(request.getParameter("topic"));
+		// save the message in ChatRoom
+		if (success) {
+			// temporarily store this message here to be sent to other
+			// active clients,
+			// such as they receive this message as soon as it arrived
+			setMessage("addTopic" + request.getParameter("topic"));
+		}
+
+		} else if ("removeTopic".equals(req)) {
+			boolean success = chatRoom.addTopic(request.getParameter("topic"));
+		// save the message in ChatRoom
+		if (success) {
+			// temporarily store this message here to be sent to other
+			// active clients,
+			// such as they receive this message as soon as it arrived
+			setMessage("removeTopic" + request.getParameter("topic"));
+		}
+		} else if ("postMessage".equals(req)) {
+			boolean success = chatRoom.addMessage(request.getParameter("topic"), request.getParameter("message"));
+			if (success) {
+				setMessage("postMessage" + request.getParameter("topic") + request.getParameter("message"));
+			}
+		} else if ("getMessages".equals(req)) {
+			String message = chatRoom.getMessages(request.getParameter("topic"));
+			out.println(message);	
+			}
+	
+		out.close();		
 	}
 	
-}
+	public synchronized String getMessage() {
+		String m = this.message;
+		this.message = null;
+		return m;
+	}
+
+	private synchronized void setMessage(String s) {
+		this.message=s;
+	}
+}	
+
